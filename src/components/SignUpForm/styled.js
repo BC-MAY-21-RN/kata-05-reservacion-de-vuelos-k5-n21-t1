0c13f79e56e495906e59b2f0581728138131bf5a
Hugoneytor <halvarez0@ucol.mx>
2021-12-21 16:04:29 -0600

    Sign Up design completed

---
+++ src/components/SignUpForm/styled.js
@@ -0,0 +1,108 @@
+//COMIENZA LO MIO -- HUGO
+import styled from 'styled-components';
+
+export const Titulo = styled.Text`
+    color: #5974F5;
+    font-size: 24px;
+    font-weight: bold;
+`
+export const InputText = styled.Text`
+    font-size: 18px;
+    font-weight: bold;
+    marginTop: 20px;
+`
+export const Instructions = styled.Text`
+    font-size: 14px;
+    font-weight: bold;  
+    color:gray; 
+`
+
+export const Block = styled.View`
+    display: flex;
+    flex: 1;
+    margin: 12px;
+`
+export const Block1 = styled.View`
+    display: flex;
+    justifyContent: center;
+    marginTop: 25px
+    marginBottom: 5px
+`
+export const Block2 = styled.View`
+    display: flex;
+    marginBottom: 30px;
+`
+export const Block3 = styled.View`
+    display: flex;
+`
+export const Block4 = styled.View`
+    display: flex;
+    margin: 30px;
+`
+export const PasswordView = styled.View`
+    flex-direction: row;
+    borderWidth: 1px;
+    alignItems: center;
+`
+export const Input = styled.TextInput`
+    height: 40px;
+    borderWidth: 1px;
+    padding: 10px;
+`
+export const InputPassword = styled.TextInput`
+    height: 40px;
+    padding: 10px;
+    flex: 1;
+`
+
+export const Eye = styled.Image`
+    width: 30px;
+    height: 30px;
+    marginRight: 10px;
+`
+export const CheckBoxView = styled.View`
+    flex-direction: row;
+    alignItems: center;
+`
+export const CheckBoxText = styled.Text`
+    font-size: 18px;
+    color:gray; 
+    textAlign: center;
+    margin: 10px;
+`
+
+export const RegisterButton = styled.Pressable`
+    display: flex;
+    flex: 1;
+    height: 40px;
+    justify-content: center;
+`
+
+export const TextButton = styled.Text`
+    fontSize: 16px;
+    fontWeight: bold;
+    color: white;
+    textAlign: center;
+`
+
+export const SignUpButton = styled.View`
+    display: flex;
+    flex-direction: row;
+    borderWidth: 1px;
+    backgroundColor: #5974F5;
+    border-radius: 10px;
+    overflow: hidden;
+    alignItems: center
+`
+export const GoogleIcon = styled.Image`
+    width: 20px;
+    height: 20px;
+    marginLeft: 40px;
+`
+
+export const LoginText = styled.Text`
+    font-size: 18px;
+    color:gray; 
+    textAlign: center;
+    marginTop: 20px;
+`
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -7,7 +7,7 @@ const Stack = createNativeStackNavigator();
 const Navigate = () => {
   return (
       <Stack.Navigator 
-          initialRouteName='Login'
+          initialRouteName='SignUp'
           screenOptions = {{
             headerShown: false
           }}>
